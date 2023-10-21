f62bf65fa1bf2310463f717ed59dba2b5bb6d366
Marcos Gomes <marcosvinicius08000@gmail.com>
2023-10-20 22:26:29 -0300

    final

--- index.js
+++ index.js
@@ -40,8 +40,74 @@ function Exibirdados(dados) {
             <td>${livro.nomeAutor}</td>
             <td>${livro.genero}</td>
             <td>${livro.isEmprestado}</td>
+            <td><button onclick="ExcluirLivro(${i})">Excluir</button></td>
         </tr>`;
     }
 }
 
+function EditarLivro(index) {
+    let dados = JSON.parse(localStorage.getItem('database')) || { livros: [] };
+    let livro = dados.livros[index];
+
+    document.getElementById('inputNome').value = livro.nome;
+    document.getElementById('inputAutor').value = livro.nomeAutor;
+    document.getElementById('genero_livro').value = livro.genero;
+    document.querySelector('input[name="emprestado"][value="' + livro.isEmprestado + '"]').checked = true;
+
+    document.getElementById('cadastrar').setAttribute('data-index', index);
+    document.getElementById('cadastrar').textContent = 'Salvar Edição';
+    
+    document.getElementById('cadastrar').onclick = function () {
+        let editedLivro = {
+            nome: document.getElementById('inputNome').value,
+            nomeAutor: document.getElementById('inputAutor').value,
+            genero: document.getElementById('genero_livro').value,
+            isEmprestado: document.querySelector('input[name="emprestado"]:checked').value
+        };
+        
+        dados.livros[index] = editedLivro;
+        localStorage.setItem('database', JSON.stringify(dados));
+
+        LimparCampos();
+        document.getElementById('cadastrar').textContent = 'Inserir';
+        document.getElementById('cadastrar').onclick = CadastrarLivro;
+        Exibirdados(dados.livros);
+    };
+}
+
+function ExcluirLivro(index) {
+    if (confirm('Tem certeza de que deseja excluir este livro?')) {
+        let dados = JSON.parse(localStorage.getItem('database')) || { livros: [] };
+        dados.livros.splice(index, 1);
+        localStorage.setItem('database', JSON.stringify(dados));
+        Exibirdados(dados.livros);
+    }
+}
+
+
+function LimparTabela() {
+    if (confirm('Tem certeza de que deseja limpar a lista de livros?')) {
+        localStorage.removeItem('database');
+        Exibirdados([]);
+    }
+}
+
+function DetalharLivro(index) {
+    let dados = JSON.parse(localStorage.getItem('database')) || { livros: [] };
+    let livro = dados.livros[index];
+    alert(`Detalhes do Livro:
+    Nome: ${livro.nome}
+    Autor: ${livro.nomeAutor}
+    Gênero: ${livro.genero}
+    Emprestado: ${livro.isEmprestado}`);
+}
+
 document.getElementById('cadastrar').addEventListener('click', CadastrarLivro);
+document.getElementById('alterar').addEventListener('click', EditarLivro);
+document.getElementById('deletar').addEventListener('click', ExcluirLivro);
+document.getElementById('Limpar').addEventListener('click', LimparTabela);
+document.getElementById('conteudo_biblioteca').addEventListener('click', function (e) {
+    if (e.target && e.target.nodeName == 'TD') {
+        DetalharLivro(e.target.parentElement.rowIndex - 1);
+    }
+});
