ee56cf401e3ab87a4210f4b5bb94b5860588708a
Marcos Gomes <marcosvinicius08000@gmail.com>
2023-10-20 23:50:27 -0300

    commit

--- index.js
+++ index.js
@@ -4,13 +4,25 @@ function CadastrarLivro() {
     let genero = document.getElementById('genero_livro').value;
     let emprestado = document.querySelector('input[name="emprestado"]:checked').value;
 
+    // Aqui você também deve coletar os novos campos
+    let edicao = document.getElementById('inputEdicao').value;
+    let ano = document.getElementById('inputAno').value;
+    let paginas = document.getElementById('inputPaginas').value;
+    let foto = document.getElementById('inputFoto').value;
+    let local = document.getElementById('inputLocal').value;
+
     let dados = JSON.parse(localStorage.getItem('database')) || { livros: [] };
 
     let novoLivro = {
         nome: nomeLivro,
         nomeAutor: nomeAutor,
         genero: genero,
-        isEmprestado: emprestado
+        isEmprestado: emprestado,
+        edicao: edicao,
+        ano: ano,
+        paginas: paginas,
+        foto: foto,
+        local: local
     };
 
     dados.livros.push(novoLivro);
@@ -23,30 +35,13 @@ function CadastrarLivro() {
     document.getElementById('genero_livro').value = '1';
     document.querySelector('input[name="emprestado"]:checked').checked = false;
 
+    document.getElementById('inputEdicao').value = '';
+    document.getElementById('inputAno').value = '';
+    document.getElementById('inputPaginas').value = '';
+    document.getElementById('inputFoto').value = '';
+    document.getElementById('inputLocal').value = '';
 
     Exibirdados(dados.livros);
-}
-
-
-function Exibirdados(dados) {
-    let tabela = document.getElementById('conteudo_biblioteca');
-    tabela.innerHTML = '';
-
-    if (dados.length === 0) {
-        tabela.innerHTML = `<tr><td colspan="5">Nenhum livro encontrado.</td></tr>`;
-    } else {
-        for (let i = 0; i < dados.length; i++) {
-            let livro = dados[i];
-            tabela.innerHTML += `<tr>
-                <td scope="row">${i + 1}</td>
-                <td>${livro.nome}</td>
-                <td>${livro.nomeAutor}</td>
-                <td>${livro.genero}</td>
-                <td>${livro.isEmprestado}</td>
-                <td><button onclick="ExcluirLivro(${i})">Excluir</button></td>
-            </tr>`;
-        }
-    }
 }
 
 function EditarLivro(index) {
@@ -58,17 +53,29 @@ function EditarLivro(index) {
     document.getElementById('genero_livro').value = livro.genero;
     document.querySelector('input[name="emprestado"][value="' + livro.isEmprestado + '"]').checked = true;
 
+    // Aqui você também deve preencher os novos campos
+    document.getElementById('inputEdicao').value = livro.edicao;
+    document.getElementById('inputAno').value = livro.ano;
+    document.getElementById('inputPaginas').value = livro.paginas;
+    document.getElementById('inputFoto').value = livro.foto;
+    document.getElementById('inputLocal').value = livro.local;
+
     document.getElementById('cadastrar').setAttribute('data-index', index);
     document.getElementById('cadastrar').textContent = 'Salvar Edição';
-    
+
     document.getElementById('cadastrar').onclick = function () {
         let editedLivro = {
             nome: document.getElementById('inputNome').value,
             nomeAutor: document.getElementById('inputAutor').value,
             genero: document.getElementById('genero_livro').value,
-            isEmprestado: document.querySelector('input[name="emprestado"]:checked').value
+            isEmprestado: document.querySelector('input[name="emprestado"]:checked').value,
+            edicao: document.getElementById('inputEdicao').value,
+            ano: document.getElementById('inputAno').value,
+            paginas: document.getElementById('inputPaginas').value,
+            foto: document.getElementById('inputFoto').value,
+            local: document.getElementById('inputLocal').value
         };
-        
+
         dados.livros[index] = editedLivro;
         localStorage.setItem('database', JSON.stringify(dados));
 
@@ -78,6 +85,36 @@ function EditarLivro(index) {
         Exibirdados(dados.livros);
     };
 }
+
+
+
+function Exibirdados(dados) {
+    let tabela = document.getElementById('conteudo_biblioteca');
+    tabela.innerHTML = '';
+
+    if (dados.length === 0) {
+        tabela.innerHTML = `<tr><td colspan="10">Nenhum livro encontrado.</td></tr>`;
+    } else {
+        for (let i = 0; i < dados.length; i++) {
+            let livro = dados[i];
+            tabela.innerHTML += `<tr>
+                <td scope="row">${i + 1}</td>
+                <td>${livro.nome}</td>
+                <td>${livro.nomeAutor}</td>
+                <td>${livro.genero}</td>
+                <td>${livro.edicao}</td>
+                <td>${livro.ano}</td>
+                <td>${livro.paginas}</td>
+                <td>${livro.foto}</td>
+                <td>${livro.local}</td>
+                <td>${livro.isEmprestado}</td>
+                <td><button onclick="ExcluirLivro(${i})">Excluir</button></td>
+            </tr>`;
+        }
+    }
+}
+
+
 
 function ExcluirLivro(index) {
     if (confirm('Tem certeza de que deseja excluir este livro?')) {
@@ -123,11 +160,19 @@ function PesquisarLivros() {
     Exibirdados(livrosFiltrados);
 }
 
+function carregarDadosDaTabela() {
+    let dados = JSON.parse(localStorage.getItem('database')) || { livros: [] };
+    Exibirdados(dados.livros);
+}
+
+
+document.addEventListener('DOMContentLoaded', carregarDadosDaTabela);
+
+
 
 document.getElementById('cadastrar').addEventListener('click', CadastrarLivro);
 document.getElementById('alterar').addEventListener('click', EditarLivro);
 document.getElementById('deletar').addEventListener('click', ExcluirLivro);
-document.getElementById('Limpar').addEventListener('click', LimparTabela);
 document.getElementById('conteudo_biblioteca').addEventListener('click', function (e) {
     if (e.target && e.target.nodeName == 'TD') {
         DetalharLivro(e.target.parentElement.rowIndex - 1);
