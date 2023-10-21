42df914867a14682e8b0308a11eec1f1da89d7f7
Marcos Gomes <marcosvinicius08000@gmail.com>
2023-10-20 22:16:20 -0300

    resolve1

--- index.js
+++ index.js
@@ -1,51 +1,46 @@
-function Gravardados() {
-    let dados = localStorage.getItem('database');
-    let json = {};
-
-    if (dados) {
-        json = JSON.parse(dados);
-    } else {
-        json = {
-            livros: []
-        };
-    }
-    return json;
-}
-
 function CadastrarLivro() {
-    let dados = Gravardados();
-
     let nomeLivro = document.getElementById('inputNome').value;
     let nomeAutor = document.getElementById('inputAutor').value;
     let genero = document.getElementById('genero_livro').value;
-    let emprestado = document.getElementById('inputEmprestado').value;
+    let emprestado = document.querySelector('input[name="emprestado"]:checked').value;
 
-    let livro = {
+    let dados = JSON.parse(localStorage.getItem('database')) || { livros: [] };
+
+    let novoLivro = {
         nome: nomeLivro,
         nomeAutor: nomeAutor,
         genero: genero,
         isEmprestado: emprestado
     };
 
-    dados.livros.push(livro);
-    Salvar(dados);
+    dados.livros.push(novoLivro);
+
+    localStorage.setItem('database', JSON.stringify(dados));
+
+
+    document.getElementById('inputNome').value = '';
+    document.getElementById('inputAutor').value = '';
+    document.getElementById('genero_livro').value = '1';
+    document.querySelector('input[name="emprestado"]:checked').checked = false;
+
+
     Exibirdados(dados.livros);
 }
 
-function Salvar(dados) {
-    localStorage.setItem('database', JSON.stringify(dados));
-}
 
 function Exibirdados(dados) {
-    let tabela = document.getElementById('biblioteca');
-    tabela.innerHTML = "";
+    let tabela = document.getElementById('conteudo_biblioteca');
+    tabela.innerHTML = '';
+
     for (let i = 0; i < dados.length; i++) {
         let livro = dados[i];
-        tabela.innerHTML += `<tr><td scope="row">${i}</td>
+        tabela.innerHTML += `<tr>
+            <td scope="row">${i + 1}</td>
             <td>${livro.nome}</td>
             <td>${livro.nomeAutor}</td>
             <td>${livro.genero}</td>
-            <td>${livro.isEmprestado}</td></tr>`;
+            <td>${livro.isEmprestado}</td>
+        </tr>`;
     }
 }
 
